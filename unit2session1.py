b8ab3cd33573e643f595ed8ea479baedd7806496
A T <amythompson0325@gmail.com>
2025-09-19 20:47:50 -0400

    finished review question

--- unit2session1.py
+++ unit2session1.py
@@ -1,1 +1,13 @@
+def all_in(a, b):
+   if (a == b):
+       return True
+   else:
+       return False
+lst_1 = [1, 2]
+lst_2 = [1, 2, 3]
+print(all_in(lst_1, lst_2))
+print(all_in(lst_2, lst_1))
+
+
+
 #TODO i plan on going ahead finishing more units before next class (already did unit 1)
