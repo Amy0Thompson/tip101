f4bfb17294c29eb61b74ce813484d8ad8ae86525
A T <amythompson0325@gmail.com>
2025-09-23 18:22:21 -0400

    i procastinated im still on unit 2

--- unit2session1.py
+++ unit2session1.py
@@ -8,6 +8,4 @@ lst_2 = [1, 2, 3]
 print(all_in(lst_1, lst_2))
 print(all_in(lst_2, lst_1))
 
-
-
-#TODO i plan on going ahead finishing more units before next class (already did unit 1)
+#TODO wait for breakout rooms (I already did the unit 1 review question)
