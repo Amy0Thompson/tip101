2cd3ba0db559b5cc5f2b4aaf04d07c48881804a7
A T <amythompson0325@gmail.com>
2025-09-23 20:53:32 -0400

    solved issue with first method. no need for "else:". also we arent checking if a == b. we are checking if each element in a is also in b.

--- unit2session1.py
+++ unit2session1.py
@@ -2,11 +2,10 @@ def all_in(a, b):
     for i in a:
         if i not in b:
             return False
-        else:
-            return True
+    return True
 lst_1 = [1, 2]
 lst_2 = [1, 2, 3]
 print(all_in(lst_1, lst_2))
-print(all_in(lst_2, lst_1)) #TODO should return false but isnt
+print(all_in(lst_2, lst_1)) 
 
 #TODO we worked on problems in breakout rooms. just did to add the solutions here
