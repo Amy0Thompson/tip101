9d75deb37fca4b318c65860c5c952a713dfe8787
A T <amythompson0325@gmail.com>
2025-09-23 20:36:47 -0400

    we worked on problems in breakout rooms. just did to add the solutions here

--- unit2session1.py
+++ unit2session1.py
@@ -1,11 +1,12 @@
 def all_in(a, b):
-   if (a == b):
-       return True
-   else:
-       return False
+    for i in a:
+        if i not in b:
+            return False
+        else:
+            return True
 lst_1 = [1, 2]
 lst_2 = [1, 2, 3]
 print(all_in(lst_1, lst_2))
-print(all_in(lst_2, lst_1))
+print(all_in(lst_2, lst_1)) #TODO should return false but isnt
 
-#TODO wait for breakout rooms (I already did the unit 1 review question)
+#TODO we worked on problems in breakout rooms. just did to add the solutions here
