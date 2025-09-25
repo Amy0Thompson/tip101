516f16cc48d875c4d33df3f54283cd32da29d25c
A T <amythompson0325@gmail.com>
2025-09-25 18:12:14 -0400

    started working again during class zoom lecture (6pm-8pm edt tuethu)

--- unit2session1.py
+++ unit2session1.py
@@ -8,4 +8,13 @@ lst_2 = [1, 2, 3]
 print(all_in(lst_1, lst_2))
 print(all_in(lst_2, lst_1)) 
 
+def create_dictionary(keys, values): 
+    d = {}
+    for i in range (len(keys)):
+        d[keys[i]] = values[i]
+    return d
+keys = ['peanut', 'dragon', 'star', 'pop', 'space']
+values = ['butter', 'fly', 'fish', 'corn', 'ship']
+print(create_dictionary(keys, values))
+
 #TODO we worked on problems in breakout rooms. just did to add the solutions here
