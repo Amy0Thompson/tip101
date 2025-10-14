becc2cb2bcb35cf70962b14787de0c325194d587
A T <amythompson0325@gmail.com>
2025-10-14 19:26:15 -0400

    unit 5

---
+++ unit5session1.py
@@ -0,0 +1,26 @@
+# class Pokemon:
+#     def init(self, name, types):
+#         self.name = name
+#         self.types = types
+#         self.is_caught = False
+#     def choose(self):
+#         if self.is_caught == True:
+#             print(self.name + " I choose you!")
+#         else:
+#             print(self.name + "is wild! Catch them if you can!")
+#     def catch(self):
+#         self.is_caught = True
+        
+#     def print_pokemon(self):
+#         print({
+#             "name": self.name,   
+#             "types": self.types, 
+#             "is_caught": self.is_caught 
+#         })
+        
+# my_pokemon = Pokemon("rattata", ["Normal"])
+# my_pokemon.print_pokemon()
+# my_pokemon.choose()
+# my_pokemon.catch()
+# my_pokemon.choose()
+#riya's code
